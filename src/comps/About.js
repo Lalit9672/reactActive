09e57cb770af66ab51cfaa00e3e72dde1e5cff91
unknown <lalitpatel0234@gmail.com>
2021-05-11 13:33:46 +0530

    first commit

---
+++ src/comps/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (        
+    <div className="router">
+           About 
+        </div>
+    )
+}
+
+export default About
---
+++ src/comps/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+    return (     
+           <div className="router">
+            Contact
+        </div>
+    )
+}
+
+export default Contact
